0e48765c3a103e604e138adba98d6289317c77f0
SysSudharsan <SysSudharsan@users.noreply.github.com>
2016-04-20 13:06:56 +0530

    Create log_forwarder.sh

---
+++ log_forwarder.sh
@@ -0,0 +1,9 @@
+# Configure Logstash-forwarder on client servers
+
+#Logstash-forwarder is a client software which ship logs to a logstash server, it should be installed on all client servers.
+
+sudo wget https://download.elastic.co/logstash-forwarder/binaries/logstash-forwarder_0.4.0_amd64.deb
+
+dpkg -i logstash-forwarder_0.4.0_amd64.deb
+
+
