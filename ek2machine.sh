a6c0427c8e3c06360354a0c8757a2d811df6e013
SysSudharsan <SysSudharsan@users.noreply.github.com>
2016-05-20 20:05:48 +0530

    Create ek2machine.sh

---
+++ ek2machine.sh
@@ -0,0 +1,156 @@
+#!/bin/bash
+sudo apt-get -y update
+
+sudo apt-get -y install wget
+
+# install Java
+
+sudo apt-get -y install openjdk-7-jre-headless
+
+
+
+# install elasticsearch
+
+wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+echo "deb http://packages.elastic.co/elasticsearch/2.x/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list
+
+sudo apt-get update
+sudo apt-get -y install elasticsearch
+
+
+# set elasticsearch to run as a service
+
+sudo service elasticsearch restart
+
+sudo update-rc.d elasticsearch defaults 95 10
+
+
+#Elasticsearch status check
+
+curl -X GET http://localhost:9200
+
+
+
+
+
+# install kibana
+
+
+echo "deb http://packages.elastic.co/kibana/4.4/debian stable main" | sudo tee -a /etc/apt/sources.list.d/kibana-4.4.x.list
+
+sudo apt-get update
+
+sudo apt-get -y install kibana
+
+sudo update-rc.d kibana defaults 96 9
+
+sudo service kibana start
+
+
+# install nginx
+
+sudo apt-get install nginx apache2-utils
+
+sudo htpasswd -c /etc/nginx/htpasswd.users kibanatestuser
+
+# sudo vi /etc/nginx/sites-available/default
+
+# server {
+ #   listen 80;
+
+ #   server_name example.com;
+
+  #  auth_basic "Restricted Access";
+   # auth_basic_user_file /etc/nginx/htpasswd.users;
+
+   # location / {
+    #    proxy_pass http://localhost:5601;
+     #   proxy_http_version 1.1;
+      #  proxy_set_header Upgrade $http_upgrade;
+       # proxy_set_header Connection 'upgrade';
+      #  proxy_set_header Host $host;
+       # proxy_cache_bypass $http_upgrade;        
+    #}
+#}
+
+sudo service nginx restart
+
+
+
+
+
+# install logstash
+
+echo "deb http://packages.elastic.co/logstash/2.2/debian stable main" | sudo tee -a /etc/apt/sources.list/logstash-2.2.x.list
+
+sudo apt-get -y update
+
+sudo apt-get -y install logstash
+
+
+
+# Create SSL Certificate
+
+sudo mkdir -p /etc/pki/tls/certs
+
+sudo mkdir /etc/pki/tls/private
+
+# Configure certificate
+
+
+# cd /etc/pki/tls; sudo openssl req -subj '/CN=ELK_server_fqdn/' -x509 -days 3650 -batch -nodes -newkey rsa:2048 -keyout private/logstash-forwarder.key -out certs/logstash-forwarder.crt
+
+
+
+
+sudo wget https://raw.githubusercontent.com/SysSudharsan/ELK/master/02-beats-input.conf > /etc/logstash/conf.d/
+
+sudo wget https://raw.githubusercontent.com/SysSudharsan/ELK/master/30-elasticsearch-output.conf > /etc/logstash/conf.d/
+
+
+
+# start logstash as a service
+
+sudo service logstash restart
+
+sudo update-rc.d logstash defaults 96 9
+
+
+# Load Kibana Dashboards
+
+cd ~
+
+curl -L -O https://download.elastic.co/beats/dashboards/beats-dashboards-1.1.0.zip
+
+sudo apt-get -y install unzip
+
+unzip beats-dashboards-*.zip
+
+cd beats-dashboards-*
+
+./load.sh
+
+
+# Load Filebeat Index Template in Elasticsearch
+
+cd ~
+
+curl -O https://gist.githubusercontent.com/thisismitch/3429023e8438cc25b86c/raw/d8c479e2a1adcea8b1fe86570e42abab0f10f364/filebeat-index-template.json
+
+curl -XPUT 'http://localhost:9200/_template/filebeat?pretty' -d@filebeat-index-template.json
+
+
+# Add Client Servers
+
+# Copy SSL Certificate
+
+scp /etc/pki/tls/certs/logstash-forwarder.crt user@client_server_private_address:/tmp
+
+
+
+
+
+
+
+
+
